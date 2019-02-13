205d80d4fe56a5e774f359010bd8fd40a8859dcd
Samantha Wong <smiwong920@gmail.com>
2019-02-13 23:08:56 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,5 @@
 color = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
 def display_rainbow(color)
-  puts "R: #{color[0]
+  puts "R: #{color[0]}, O: #{color[1]}, Y: #{person[2]}"
 end 
